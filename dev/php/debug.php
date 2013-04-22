b5aac39ccb7d5f634cdd0a5588724811c97074a9
Michael Garde <mgarde@gmail.com>
2013-04-22 17:39:41 -0400

    Nightly

--- dev/php/debug.php
+++ dev/php/debug.php
@@ -5,20 +5,16 @@
  *
  * @author  Mike Garde
  *
- * @param array   $array  Array you want to see.
- * @param boolean $die    Should this kill the process when done?
- * @param boolean $return Do you want this echoed or returned
+ * @param array    $array   Array you want to see.
+ * @param boolean  $die     Should this kill the process when done?
+ * @param boolean  $return  Do you want this echoed or returned
  *
  * @return string A view of an array but formatted for easy reading via HTML.
  */
-function print_a($array, $die = true, $return = false) {
+function print_a($array, $die=true, $return=false) {
 
 	if(!$array) {
 		$array = $GLOBALS;
-		foreach ($array as &$item){
-			if(is_array($item) || is_object($item))
-				$item = null;
-		}
 	}
 
 	$array = object_to_array($array);
@@ -34,13 +30,11 @@ function print_a($array, $die = true, $return = false) {
 	$string.= '<link rel="stylesheet" type="text/css" href="//google-code-prettify.googlecode.com/svn/trunk/src/prettify.css">';
 	$string.= '<link href="//fonts.googleapis.com/css?family=Ubuntu+Mono" rel="stylesheet" type="text/css">';
 	$string.= '<style>';
-	$string.= 'pre { font-family: \'Ubuntu Mono\', sans-serif; }';
+	$string.= 'pre { background-color: #fff; font-family: \'Ubuntu Mono\', sans-serif; }';
 	$string.= 'li.L0, li.L1, li.L2, li.L3, li.L5, li.L6, li.L7, li.L8 { list-style-type: decimal; }';
 	$string.= 'ol { padding: 0 0 0 45px; }';
 	$string.= '</style>';
-	$string.= '<pre class="prettyprint linenums">';
-	$string.= $array;
-	$string.= '</pre>';
+	$string.= '<pre class="prettyprint linenums">'. $array .'</pre>';
 	$string.= '<script>prettyPrint();</script>';
 
 	if ($return) {
@@ -59,15 +53,67 @@ function print_a($array, $die = true, $return = false) {
  *
  * @author  Mike Garde
  *
- * @param object  $object Object you want to make into an array
+ * @param object   $object    Object you want to make into an array
+ * @param boolean  $truncate  Object you want to make into an array
  *
  * @return An Array!
  */
-function object_to_array($object) {
+function object_to_array($object, $truncate=false) {
 	foreach($object as $key => $value) {
-		if(is_object($object)) {
-			$object[$key] = self::object_to_array($value);
+		if(is_object($object) || is_array($object)) {
+			$object[$key] = object_to_array($value);
+		} else {
+			$object = ($truncate) ? truncate($value) : $value;
 		}
 	}
-	return (array)$object;
+	return $object;
+}
+
+
+/**
+ * Truncate a string
+ *
+ * @author  Mike Garde
+ *
+ * @param string   $string  String you want to truncate
+ * @param number   $len     Leingth you wish to trunicate the string to
+ * @param string   $append  What should be appended to the end of the string
+ * @param boolean  $strict  True  = will not excede the $len param
+ *                          False = will find the trunication point and move to the end of the previous word using char,
+ *                                  OR the end of the word (using word),
+ *                                  OR the end of the current sentence (using sentence)
+ *
+ * @param string   $type    'char'     = count the number of characters
+ *                          'word'     = count the number of words
+ *							'sentence' = counts characters but looks for the end of a sentence
+ *
+ * @return A trunicated string
+ */
+function truncate($string, $len=137, $append='...', $strict=true) {
+
+	if($strict)
+		$return = substr($string, 0, $len);
+
+	return $return.$append;
+}
+
+
+function generate_large_array($size=60, $depth=2) {
+	//echo 'generating large array: '.$size.' / '.$depth.'<br>';
+	$i = 1;
+
+	while ($i <= $size) {
+		if($depth == 0) {
+			$value = rand(5, 99999);
+			//echo $i.' / '. $depth .' : '. $value .'<br>';
+			$return[$i] = $value;
+		} else {
+			$new_size = round(($size / ($depth + 3)), 0, PHP_ROUND_HALF_UP);
+			if($new_size < 3)
+				$new_size = 3;
+			$return[$i] = generate_large_array($new_size, $depth-1);
+		}
+		$i++;
+	}
+	return $return;
 }
