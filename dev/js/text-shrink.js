d30e114ba11313271592ef1833b9de026d69d661
Michael Garde <mgarde@gmail.com>
2013-05-06 14:51:00 -0400

    Finalizing basic version of Shrink Text

--- dev/js/text-shrink.js
+++ dev/js/text-shrink.js
@@ -14,41 +14,40 @@ function num_of_lines(e) {
 	return lines;
 }
 
-var shrink_timer;
-function shrink_it(shrink_element){
-
-	var lines = num_of_lines(shrink_element);
-	console.log('lines: ' + lines);
-	if (lines < 1) {
-		console.log('clearing timer');
-		clearInterval(shrink_timer);
-		return true;
-	} else {
-		//var shrink_timer = setInterval(shrink_it(shrink_element), 1000);
-	}
-
-	var cur_size = parseInt(shrink_element.css('font-size'));
-	var new_size = cur_size - 1;
-	shrink_element.css('font-size', new_size);
-
-	console.log('cur_size: ' + cur_size);
-	console.log('new_size: ' + new_size);
-
-	var cur_size = parseInt(shrink_element.css('line-height'));
-	var new_size = cur_size - 1;
-	shrink_element.css('line-height', new_size+'px');
-
-	console.log('cur_size: ' + cur_size);
-	console.log('new_size: ' + new_size);
-
-	console.log('----------------');
-
-}
-
+var shrink_timer = new Array;
 $('.s-shrink').each(function(index){
 
-	shrink_timer = setInterval(shrink_it($(this)), 500);
+	var shrink_element = $(this);
+	shrink_timer[index] = setInterval(function(){
 
+		var lines = num_of_lines(shrink_element);
+		console.log('lines: ' + lines);
+		if (lines <= 1) {
+			console.log('clearing timer');
+			clearInterval(shrink_timer[index]);
+			return true;
+		} else {
+			//var shrink_timer = setInterval(shrink_it(shrink_element), 1000);
+		}
+
+		var cur_size = parseInt(shrink_element.css('font-size'));
+		var new_size = cur_size - 1;
+		shrink_element.css('font-size', new_size);
+
+		console.log('cur_size: ' + cur_size);
+		console.log('new_size: ' + new_size);
+
+		var cur_size = parseInt(shrink_element.css('line-height'));
+		var new_size = cur_size - 1;
+		shrink_element.css('line-height', new_size+'px');
+
+		console.log('cur_size: ' + cur_size);
+		console.log('new_size: ' + new_size);
+
+		console.log('----------------');
+
+	}, 10);
+	
 });
 
 $('.s-shrink').click(function(index){
