4a64949039af7084197c145a7d4c79b811b0d5a9
Michael Garde <mgarde@gmail.com>
2013-06-07 14:01:55 -0400

    Finalizing Lazy Images

--- dev/js/lazy_images.js
+++ dev/js/lazy_images.js
@@ -4,13 +4,13 @@
 // http://benpickles.github.com/onScreen
 //
 // Released under MIT license.
-;(function($) {
-  $.expr[":"].onScreen = function(elem) {
-    var $window = $(window)
+;(function(jQuery) {
+  jQuery.expr[":"].onScreen = function(elem) {
+    var $window = jQuery(window)
     var viewport_top = $window.scrollTop()
     var viewport_height = $window.height()
     var viewport_bottom = viewport_top + viewport_height
-    var $elem = $(elem)
+    var $elem = jQuery(elem)
     var top = $elem.offset().top
     var height = $elem.height()
     var bottom = top + height
@@ -26,16 +26,15 @@
 
 window.onload=function() {
 
-$('img:onScreen[data-src]').each(function(){
-	$(this).attr('src', $(this).attr('data-src'));
-	$(this).removeAttr('data-src');
+jQuery('img:onScreen[data-src]').each(function(){
+	jQuery(this).attr('src', jQuery(this).attr('data-src'));
+	jQuery(this).removeAttr('data-src');
 });
 
-$(document).scroll(function(){
-	console.log('scroll happened');
-	$('img:onScreen[data-src]').each(function(){
-		$(this).attr('src', $(this).attr('data-src')).stop(true,true).hide().fadeIn(300);
-		$(this).removeAttr('data-src');
+jQuery(document).scroll(function(){
+	jQuery('img:onScreen[data-src]').each(function(){
+		jQuery(this).attr('src', jQuery(this).attr('data-src')).stop(true,true).hide().fadeIn(300);
+		jQuery(this).removeAttr('data-src');
 	});
 });
 	
