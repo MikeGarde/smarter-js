958b7ad73b2953ec840f2dd9a30576ffee6aa250
Michael Garde <mgarde@gmail.com>
2013-06-12 16:53:07 -0400

    Adding "window.getComputedStyle" support

--- dev/js/base.js
+++ dev/js/base.js
@@ -105,3 +105,22 @@ if ( !Array.prototype.forEach ) {
 		// 8. return undefined
 	};
 }
+
+// Gives the final used values of all the CSS properties of an element.
+// Example from http://snipplr.com/view/13523/
+if (!window.getComputedStyle) {
+    window.getComputedStyle = function(el, pseudo) {
+        this.el = el;
+        this.getPropertyValue = function(prop) {
+            var re = /(\-([a-z]){1})/g;
+            if (prop == 'float') prop = 'styleFloat';
+            if (re.test(prop)) {
+                prop = prop.replace(re, function () {
+                    return arguments[2].toUpperCase();
+                });
+            }
+            return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+        }
+        return this;
+    }
+}
